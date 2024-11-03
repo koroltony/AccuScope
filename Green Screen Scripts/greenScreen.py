e8d05d58abbdd776496621fa58d998ba2ad0d6f9
Zion Chang <zionchang@ucsb.edu>
2024-11-03 07:57:10 -0800

    Update greenScreen.py
    Added minimap functionality, but is still slow due to if statements.

--- Green Screen Scripts/greenScreen.py
+++ Green Screen Scripts/greenScreen.py
@@ -1,6 +1,7 @@
 import cv2
 import os
 import time
+import numpy as np
 
 start_time = time.time()
 
@@ -11,27 +12,41 @@ def checkGreen(video):
     height = round(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
     BRthreshold = 50
     Gthreshold = 100
+    miniMapTopLeft = np.array([63, 35])
+    miniMapBottomRight = np.array([416, 664])
+    miniMapMiddleCord = np.add(miniMapTopLeft, np.subtract(miniMapBottomRight, miniMapTopLeft)//2)
     if not video.isOpened():
         print("Video could not be opened")
 
     while video.isOpened(): 
         currentFrame = video.get(cv2.CAP_PROP_POS_FRAMES)
-        print(round(currentFrame*100/totalFrames, 2),"% Done After", round(time.time() - start_time, 2), " Seconds")
+        #print(round(currentFrame*100/totalFrames, 2),"% Done After", round(time.time() - start_time, 2), " Seconds")
         #frameRead is whether the frame was successfully read
         frameRead, frame = video.read()
-
+        
         #Break if Frame was Read Unsuccessfully
         if not frameRead:
             break
         
+        #Resize from 4K into 1080p (My Monitor Only Supports 1080p)
+        #displayFrame = cv2.resize(frame, (960, 540))
+
+        #Show the Individual Frame
+        #cv2.imshow('Surgery Video', displayFrame)
+
+        #Minimap exists from (35, 63) and ends at (664, 416)
         #Get R, G, B pixel value in the middle of each frame
         Bframemiddle = frame[height//2, width//2, 0]
         Gframemiddle = frame[height//2, width//2, 1]
         Rframemiddle = frame[height//2, width//2, 2]
-        
+        Bminimapmiddle = frame[miniMapMiddleCord[1], miniMapMiddleCord[0], 0]
+        Gminimapmiddle = frame[miniMapMiddleCord[1], miniMapMiddleCord[0], 1]
+        Rminimapmiddle = frame[miniMapMiddleCord[1], miniMapMiddleCord[0], 2]
 
         if(Bframemiddle < BRthreshold) and (Gframemiddle > Gthreshold) and (Rframemiddle < BRthreshold):
             print('Non Minimap Green Screen Error Found at: ', round((currentFrame/fps), 4), 'seconds')
+        elif(Bminimapmiddle < BRthreshold) and (Gminimapmiddle > Gthreshold) and (Rminimapmiddle < BRthreshold):
+            print('Minimap Green Screen Error Found at: ', round((currentFrame/fps), 4), 'seconds')
 
         #print("frame: ", currentFrame, " time: ", round(currentFrame/fps, 2))
 
