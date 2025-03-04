bc3102c018769d206e2ebbfd11b4a43226deb5c7
Zion Chang <zionchang@ucsb.edu>
2025-03-04 14:37:05 -0800

    finished Menu Detection Script

--- OpticalCharacterRecognition/menuDetect.py
+++ OpticalCharacterRecognition/menuDetect.py
@@ -1,63 +1,25 @@
-import cv2 as cv
+#Function takes in a matrix of grayscale pixels and a folder path to reference kernels
+#returns a boolean if a menu exists inside.
+#The Fast Fourier Transform is used to speed up operations.
+
+import cv2
 import numpy as np
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
+import os
 
-# Read the images
-img = cv.imread('OpticalCharacterRecognition/testImages/frame2.jpg', cv.IMREAD_GRAYSCALE)
-assert img is not None, "file could not be read, check with os.path.exists()"
-template = cv.imread('OpticalCharacterRecognition/menuImages/Success.jpg', cv.IMREAD_GRAYSCALE)
-assert template is not None, "file could not be read, check with os.path.exists()"
+def hasMenu(frame, kernelsPath):
+    greyFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    menuThresholds = []
+    for filename in os.listdir(kernelsPath):
+        fullPath = os.path.join(kernelsPath, filename)
+        if os.path.isfile(fullPath):
+            grayKernel = cv2.imread(fullPath, cv2.IMREAD_GRAYSCALE)
+            res = cv2.matchTemplate(greyFrame, grayKernel, cv2.TM_CCOEFF_NORMED)
+            _, max_val, _, max_loc = cv2.minMaxLoc(res)
+            menuThresholds.append(max_val)
+    print(menuThresholds)
+    menuThresholds = np.array(menuThresholds)
+    return np.any(menuThresholds > 0.9)
 
-w, h = template.shape[::-1]
-
-# Define the methods
-methods = ['TM_CCORR_NORMED']
-
-fig, axes = plt.subplots(2, len(methods) + 1, figsize=(3 * (len(methods) + 1), 6))  # Extra column for labels
-
-# Set the first column labels
-axes[0, 0].text(0.5, 0.5, "Matching Result", fontsize=12, ha='center', va='center', rotation=90)
-axes[1, 0].text(0.5, 0.5, "Detected Point", fontsize=12, ha='center', va='center', rotation=90)
-
-# Remove axis for label cells
-axes[0, 0].axis('off')
-axes[1, 0].axis('off')
-
-# Loop through methods and plot results
-for i, meth in enumerate(methods):
-    img_copy = img.copy()
-    method = getattr(cv, meth)
-
-    # Apply template matching
-    res = cv.matchTemplate(img_copy, template, method)
-
-    print(res)
-
-    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
-
-    print(max_val)
-    print(min_val)
-
-    # Determine the best match location
-    if method in [cv.TM_SQDIFF, cv.TM_SQDIFF_NORMED]:
-        top_left = min_loc
-    else:
-        top_left = max_loc
-    bottom_right = (top_left[0] + w, top_left[1] + h)
-
-    # Draw rectangle around detected template
-    cv.rectangle(img_copy, top_left, bottom_right, (255), 2)
-
-    # Add method name to the top row
-    axes[0, i + 1].set_title(meth, fontsize=10)
-
-    # Plot matching result
-    axes[0, i + 1].imshow(res, cmap='gray')
-    axes[0, i + 1].axis('off')
-
-    # Plot detected template in the original image
-    axes[1, i + 1].imshow(img_copy, cmap='gray')
-    axes[1, i + 1].axis('off')
-
-plt.tight_layout()
-plt.show()
+#frame = cv2.imread('OpticalCharacterRecognition/testImages/frame2.jpg')
+#print(hasMenu(frame, 'OpticalCharacterRecognition/menuImages'))
