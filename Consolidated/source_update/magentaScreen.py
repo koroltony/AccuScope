ce7fa5330c6f7221035d04e34b600a224ed2965a
TonyKorol1 <tkorol@ucsb.edu>
2025-04-29 18:26:19 -0700

    magenta update

--- Consolidated/source_update/magentaScreen.py
+++ Consolidated/source_update/magentaScreen.py
@@ -1,30 +1,34 @@
-import cv2
-import os
 import time
 import numpy as np
 from numba import njit
 
 start_time = time.time()
 
-def checkMagentaFrame(frame):
+def checkMagentaFrame(frame, threshold_ratio=0.1):
 
-    # Magenta Logical Mask:
+    magenta_mask = (frame[:, :, 2] > 120) & (frame[:, :, 1] < 50) & (frame[:, :, 0] > 120)
 
-    Magenta_condition = np.any((frame[:, :, 2] > 120) & (frame[:, :, 1] < 50) & (frame[:, :, 0] > 120))
 
-    if Magenta_condition:
-        return 1
+    magenta_count = np.sum(magenta_mask)
 
-    else:
-        return 0
-    
+    total_pixels = 640*480
+
+    # If the proportion of magenta pixels is greater than a certain value:
+    return 1 if magenta_count / total_pixels > threshold_ratio else 0
+
 @njit
-def checkMagentaFrame_numba(frame):
-    for i in range(frame.shape[0]):
-        for j in range(frame.shape[1]):
+def checkMagentaFrame_numba(frame, threshold_ratio=0.1):
+    count = 0
+    total_pixels = 640*480
+    threshold = total_pixels * threshold_ratio
+
+    for i in range(480):
+        for j in range(640):
             r = frame[i, j, 2]
             g = frame[i, j, 1]
             b = frame[i, j, 0]
             if r > 120 and g < 50 and b > 120:
-                return 1
+                count += 1
+                if count > threshold:
+                    return 1
     return 0
